ca764167c3f6c037f0072a9d852ec3d6b110f217
Alex Grover <hello@alexgrover.me>
2024-11-04 21:34:09 -0500

    Farcaster

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,6 +1,15 @@
-import { Text, View } from 'react-native'
+import { useLoginWithFarcaster } from '@privy-io/expo'
+import { useCallback } from 'react'
+import { Button, Text, View } from 'react-native'
 
 export default function Index() {
+  const { loginWithFarcaster, state } = useLoginWithFarcaster()
+
+  const handlePress = useCallback(
+    () => loginWithFarcaster({ relyingParty: 'com.checkmate.app' }),
+    [loginWithFarcaster],
+  )
+
   return (
     <View
       style={{
@@ -9,7 +18,19 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button
+        title="Sign in with Farcaster"
+        onPress={handlePress}
+        disabled={state.status === 'polling-status'}
+      />
+      {state.status === 'error' && (
+        <>
+          <Text style={{ color: 'red' }}>There was an error</Text>
+          {state.error && (
+            <Text style={{ color: 'lightred' }}>{state.error.message}</Text>
+          )}
+        </>
+      )}
     </View>
   )
 }
