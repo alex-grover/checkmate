ccf1b088417eb529d6c762b81d21a8d14c9294d7
Alex Grover <hello@alexgrover.me>
2024-11-04 21:25:21 -0500

    Privy

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,9 +1,15 @@
+import { PrivyProvider } from '@privy-io/expo'
 import { Stack } from 'expo-router'
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <PrivyProvider
+      appId="cm326luuf00lhq82pedgywxsv"
+      clientId="client-WY5dQjhgviLzhxDGjxNyZASXhW5VXHCWmrmdrbmXtNBDZ"
+    >
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </PrivyProvider>
   )
 }
